f27e3f39f633454ade73882a0e1371dd4f1041c0
GIanluca <gianogli@tiscali.it>
2021-12-05 00:13:24 +0100

    Updater: Always disable UpdateCheck
    tetst

--- src/com/crdroid/updater/misc/Utils.java
+++ src/com/crdroid/updater/misc/Utils.java
@@ -393,7 +393,7 @@ public class Utils {
     }
 
     public static boolean isUpdateCheckEnabled(Context context) {
-        return getUpdateCheckSetting(context) != Constants.AUTO_UPDATES_CHECK_INTERVAL_NEVER;
+        return false;
     }
 
     public static long getUpdateCheckInterval(Context context) {
