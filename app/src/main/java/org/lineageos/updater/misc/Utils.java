bbdd538c24879f9c1449e2bab54dd1e93135f8fe
gianogli <gianogli@tiscali.it>
2023-09-16 18:01:19 +0200

    Updater: Disable Auto Update Check
    Until my OTA will be available

--- app/src/main/java/org/lineageos/updater/misc/Utils.java
+++ app/src/main/java/org/lineageos/updater/misc/Utils.java
@@ -411,7 +411,7 @@ public class Utils {
     }
 
     public static boolean isUpdateCheckEnabled(Context context) {
-        return getUpdateCheckSetting(context) != Constants.AUTO_UPDATES_CHECK_INTERVAL_NEVER;
+        return false;
     }
 
     public static long getUpdateCheckInterval(Context context) {
